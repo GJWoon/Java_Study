dbbeb29a3678e82b0a44e7292802fa88fcb2f208
GJWoon <guemjiwoon@gmail.com>
2022-07-15 18:27:11 +0900

    Ch26 static 메서드와 인스턴스 메서드

--- src/main/java/com/study/java_study/ch_06/ReferenceParamEx.java
+++ src/main/java/com/study/java_study/ch_06/ReferenceParamEx.java
@@ -3,6 +3,9 @@ package com.study.java_study.ch_06;
 
 public class ReferenceParamEx {
 
+
+    public int a = 0;
+
     public static void main(String[] args) {
 
         Data data = new Data();
@@ -11,7 +14,8 @@ public class ReferenceParamEx {
 
         change(data);
 
-        System.out.println(data.x);
+        System.out.println("change Afger x = " + data.x);
+
     }
 
     static void change(Data d){
---
+++ src/main/java/com/study/java_study/ch_06/StaticMethod.java
@@ -0,0 +1,48 @@
+package com.study.java_study.ch_06;
+
+public class StaticMethod {
+
+    public int x = 0;
+
+    public static int staticX =0;
+
+    // static 메서드에서 static 변수를 사용하였기 때문에 에러가 나지 않는다.
+    public static void changeXStaticMethod(int a){
+        staticX = a;
+
+    }
+    // 인스턴스 메서드는 인스턴스 변수를 사용 할 수 있기 때문에 오류가 나지 않는다.
+    public void changeX(int a){
+        x = 1;
+    }
+
+}
+
+class StaticMethodTest{
+
+    public static void main(String[] args) {
+
+        int x = 2;
+
+        //에러!!
+        //StaticMethod.changeX(1);
+
+        // static 메서드 사용방법
+        StaticMethod.changeXStaticMethod(2);
+
+        System.out.println(StaticMethod.staticX);
+
+
+        // 인스턴스 메서드 사용방법
+        StaticMethod staticMethod = new StaticMethod();
+
+        staticMethod.changeX(2);
+
+        System.out.println(staticMethod.x);
+
+    }
+
+
+
+
+}
